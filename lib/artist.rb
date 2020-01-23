94d8430712cc0467a3b21cd9aaef899225ab398d
Terren Culberson <culbersonterren@yahoo.com>
2020-01-23 03:45:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,5 +17,13 @@ class Artist
       @songs << song
   end
   
-  
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+
 end
